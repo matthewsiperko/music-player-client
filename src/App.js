54b4d2175f5b4f2370c9791a0d414282e3594254
msiperko <matthewsiperko@gmail.com>
2021-06-11 19:18:39 -0400

    added play/pause functionality

--- src/App.js
+++ src/App.js
@@ -10,12 +10,17 @@ import data from './util'
 function App() {
   //State
   const [songs, setSongs] = useState(data());
-  const [currentSong, setCurrentSong] = useState(songs[0])
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false)
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <h1></h1>
       <Song currentSong={currentSong} />
-      <Player />
+      <Player 
+      setIsPlaying = {setIsPlaying}
+      isPlaying = {isPlaying}
+      currentSong={currentSong} 
+      />
     </div>
   );
 }
