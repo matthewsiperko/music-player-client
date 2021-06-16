8269e37c66c33c421897c498f9ff984ddea4dfdb
msiperko <matthewsiperko@gmail.com>
2021-06-16 19:14:45 -0400

    added song skip capability

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,18 @@
-import React, { useState, useRef } from 'react'
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { faPlay,faPause, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 
-const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
-    //Ref
-    const audioRef = useRef(null)
+const Player = ({ 
+    audioRef, 
+    currentSong, 
+    isPlaying, 
+    setIsPlaying, 
+    setSongInfo, 
+    songInfo, 
+    songs, 
+    setCurrentSong 
+}) => {
     //Event Handlers
     const playSongHandler = () => {
         if(isPlaying){
@@ -17,48 +24,59 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
         }
     }
     //Event Handlers
-    const timeUpdateHandler = (e) => {
-        const current = e.target.currentTime
-        const duration = e.target.duration
-        setSongInfo({ ...songInfo, currentTime: current, duration })
-    }
     const getTime = (time) => {
         return(
-            Math.floor(time / 60) + ':' + ('0' + Math.floor( time % 60)).slice(-2)
+            Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2)
         )
     }
     const dragHandler = (e) => {
         audioRef.current.currentTime = e.target.value
         setSongInfo({...songInfo, currentTime: e.target.value})
     }
-    //State
-    const [songInfo, setSongInfo] = useState({
-        currentTime: 0,
-        duration: 0
-    })
+    const skipTrackHandler = (direction) => {
+        let currentIndex = songs.findIndex((song) => song.id === currentSong.id)
+        if(direction === 'skip-forward'){
+            setCurrentSong(songs[(currentIndex + 1) % songs.length])
+            console.log(currentIndex)
+        }else {
+            if((currentIndex - 1) % songs.length === -1) {
+                setCurrentSong(songs[songs.length -1])
+                return;
+            }
+            setCurrentSong(songs[(currentIndex - 1) % songs.length])
+        }
+    }
     return(
         <div className='player'>
             <div className="time-control">
                 <p>{getTime(songInfo.currentTime)}</p>
                 <input
                     min={0}
-                    max={songInfo.duration}
+                    max={songInfo.duration || 0}
                     value={songInfo.currentTime}
                     onChange={dragHandler}
                     type="range"/>
-                <p>{getTime(songInfo.duration)}</p>
+                <p>{getTime(songInfo.duration || 0)}</p>
             </div>
             <div className="play-control">
-                <FontAwesomeIcon className='skip-back' size='2x' icon={faAngleLeft} />
-                <FontAwesomeIcon onClick={playSongHandler} className='play' size='2x' icon={faPlay} />
-                <FontAwesomeIcon className='skip-forward' size='2x' icon={faAngleRight} />
+                <FontAwesomeIcon
+                    onClick={() => skipTrackHandler('skip-back')}
+                    className='skip-back'
+                    size='2x'
+                    icon={faAngleLeft}
+                />
+                <FontAwesomeIcon
+                    onClick={playSongHandler}
+                    className='play' size='2x'
+                    icon={isPlaying ? faPause : faPlay}
+                />
+                <FontAwesomeIcon
+                    onClick={() => skipTrackHandler('skip-forward')}
+                    className='skip-forward'
+                    size='2x'
+                    icon={faAngleRight}
+                />
             </div>
-            <audio
-                onTimeUpdate={timeUpdateHandler}
-                onLoadedMetadata={timeUpdateHandler}
-                ref={audioRef}
-                src={currentSong.audio}>
-            </audio>
         </div>
     )
 }
